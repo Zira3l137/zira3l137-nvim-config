858c0ae145def9742439453037700f163b936043
Zira3l137 <raulbairamov2002@gmail.com>
2026-03-24 16:12:15 +0400

    fix: reconfigure plugins

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -4,28 +4,75 @@
 ---@module 'lazy'
 ---@type LazySpec
 return {
+  {
     'nvim-neo-tree/neo-tree.nvim',
     version = '*',
     dependencies = {
-        'nvim-lua/plenary.nvim',
-        'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-        'MunifTanjim/nui.nvim',
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
     },
     lazy = false,
     keys = {
-        { '<leader>e', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+      { '<leader>e', ':Neotree toggle<CR>', desc = 'NeoTree reveal', silent = true },
     },
     ---@module 'neo-tree'
     ---@type neotree.Config
     opts = {
-        filesystem = {
-            window = {
-                mappings = {
-                    ['<leader>e'] = 'close_window',
-                    ['l'] = 'open_tabnew',
-                    ['h'] = 'close_node',
-                },
-            },
+      popup_border_style = 'rounded',
+      default_component_configs = {
+        git_status = {
+          symbols = {
+            added = '+',
+            modified = '~',
+            deleted = '✕',
+            renamed = '󰁔',
+            untracked = '?',
+            ignored = '_',
+            unstaged = '○',
+            staged = '✓',
+            conflict = '!',
+          },
         },
+      },
+      filesystem = {
+        bind_to_cwd = true,
+        hijack_netrw_behavior = 'open_current',
+        use_libuv_file_watcher = true,
+        window = {
+          mappings = {
+            ['l'] = 'open',
+            ['h'] = 'close_node',
+          },
+        },
+      },
     },
+  },
+  {
+    'antosha417/nvim-lsp-file-operations',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-neo-tree/neo-tree.nvim', -- makes sure that this loads after Neo-tree.
+    },
+    config = function() require('lsp-file-operations').setup() end,
+  },
+  {
+    's1n7ax/nvim-window-picker',
+    version = '2.*',
+    config = function()
+      require('window-picker').setup {
+        filter_rules = {
+          include_current_win = false,
+          autoselect_one = true,
+          -- filter using buffer options
+          bo = {
+            -- if the file type is one of following, the window will be ignored
+            filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
+            -- if the buffer type is one of following, the window will be ignored
+            buftype = { 'terminal', 'quickfix' },
+          },
+        },
+      }
+    end,
+  },
 }
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,20 +1,52 @@
 ---@module 'lazy'
 ---@type LazySpec
 return {
-    "folke/snacks.nvim",
-    opts = {
-        indent = {
-            enabled = true,
-            chunk = {
-                enabled = true,
-                char = {
-                    corner_top = "╭",
-                    corner_bottom = "╰",
-                    horizontal = "─",
-                    vertical = "│",
-                    arrow = ">",
-                },
-            },
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  init = function()
+    vim.api.nvim_create_autocmd('VimEnter', {
+      callback = function() Snacks.dim() end,
+    })
+  end,
+  opts = {
+    dim = {
+      enabled = true,
+      scope = {
+        min_size = 5,
+        max_size = 20,
+        siblings = true,
+      },
+      animate = {
+        enabled = vim.fn.has 'nvim-0.10' == 1,
+        easing = 'outQuad',
+        duration = {
+          step = 20, -- ms per step
+          total = 300, -- maximum duration
         },
+      },
+      -- what buffers to dim
+      filter = function(buf) return vim.g.snacks_dim ~= false and vim.b[buf].snacks_dim ~= false and vim.bo[buf].buftype == '' end,
     },
+    scroll = {
+      enabled = true,
+      animate = {
+        duration = { step = 10, total = 100 },
+        easing = 'linear',
+      },
+    },
+    indent = {
+      enabled = true,
+      chunk = {
+        enabled = true,
+        char = {
+          corner_top = '╭',
+          corner_bottom = '╰',
+          horizontal = '─',
+          vertical = '│',
+          arrow = '>',
+        },
+      },
+    },
+  },
 }
