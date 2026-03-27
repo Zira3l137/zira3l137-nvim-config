b2b6f22cfec96e6ce82b16f59c6d6f56f7491bd8
Zira3l137 <raulbairamov2002@gmail.com>
2026-03-27 22:15:08 +0400

    fix: switch theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,67 +1,65 @@
 ---@module 'lazy'
 ---@type LazySpec
 return {
-    { -- You can easily change to a different colorscheme.
-        -- Change the name of the colorscheme plugin below, and then
-        -- change the command in the config to whatever the name of that colorscheme is.
+  { -- You can easily change to a different colorscheme.
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is.
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'scottmckendry/cyberdream.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('cyberdream').setup {
+        -- Set light or dark variant
+        variant = 'dark', -- use "light" for the light variant. Also accepts "auto" to set dark or light colors based on the current value of `vim.o.background`
+
+        -- Enable transparent background
+        transparent = true,
+
+        -- Reduce the overall saturation of colours for a more muted look
+        saturation = 1, -- accepts a value between 0 and 1. 0 will be fully desaturated (greyscale) and 1 will be the full color (default)
+
+        -- Enable italics comments
+        italic_comments = true,
+
+        -- Replace all fillchars with ' ' for the ultimate clean look
+        hide_fillchars = false,
+
+        -- Apply a modern borderless look to pickers like Telescope, Snacks Picker & Fzf-Lua
+        borderless_pickers = false,
+
+        -- Set terminal colors used in `:terminal`
+        terminal_colors = true,
+
+        -- Improve start up time by caching highlights. Generate cache with :CyberdreamBuildCache and clear with :CyberdreamClearCache
+        cache = true,
+
+        -- Disable or enable colorscheme extensions
+        extensions = {
+          telescope = true,
+          notify = true,
+          mini = true,
+          gitsigns = true,
+        },
+
+        -- Alternatively, you can use 'default' to set all extensions at once
+        -- cache = true, -- Use cache for fastest loads
+        -- extensions = {
+        --     default = false, -- Disable all by default
+        --     base = true, -- Enable all built-in hl groups (you probably want this)
         --
-        -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-        'scottmckendry/cyberdream.nvim',
-        priority = 1000, -- Make sure to load this before all the other start plugins.
-        config = function()
-            ---@diagnostic disable-next-line: missing-fields
-            require("cyberdream").setup({
-                -- Set light or dark variant
-                variant = "dark", -- use "light" for the light variant. Also accepts "auto" to set dark or light colors based on the current value of `vim.o.background`
-
-                -- Enable transparent background
-                transparent = true,
-
-                -- Reduce the overall saturation of colours for a more muted look
-                saturation = 1, -- accepts a value between 0 and 1. 0 will be fully desaturated (greyscale) and 1 will be the full color (default)
-
-                -- Enable italics comments
-                italic_comments = true,
-
-                -- Replace all fillchars with ' ' for the ultimate clean look
-                hide_fillchars = false,
-
-                -- Apply a modern borderless look to pickers like Telescope, Snacks Picker & Fzf-Lua
-                borderless_pickers = false,
-
-                -- Set terminal colors used in `:terminal`
-                terminal_colors = true,
-
-                -- Improve start up time by caching highlights. Generate cache with :CyberdreamBuildCache and clear with :CyberdreamClearCache
-                cache = true,
-
-
-
-                -- Disable or enable colorscheme extensions
-                extensions = {
-                    telescope = true,
-                    notify = true,
-                    mini = true,
-                    gitsigns = true,
-                },
-
-                -- Alternatively, you can use 'default' to set all extensions at once
-                -- cache = true, -- Use cache for fastest loads
-                -- extensions = {
-                --     default = false, -- Disable all by default
-                --     base = true, -- Enable all built-in hl groups (you probably want this)
-                --
-                --     -- Now enable only what you want to use
-                --     telescope = true,
-                --     cmp = true,
-                --     gitsigns = true,
-                -- },
-            })
-            -- Load the colorscheme here.
-            -- Like many other themes, this one has different styles, and you could load
-            -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-            vim.cmd.colorscheme 'cyberdream'
-        end,
-    },
+        --     -- Now enable only what you want to use
+        --     telescope = true,
+        --     cmp = true,
+        --     gitsigns = true,
+        -- },
+      }
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'cyberdream'
+    end,
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
