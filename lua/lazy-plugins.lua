eff19bcbdbc964f2772db7e584f704d86afa231e
Zira3l137 <raulbairamov2002@gmail.com>
2026-03-27 22:13:31 +0400

    add: more plugins

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -35,6 +35,8 @@ require('lazy').setup({
   require 'plugins.toggleterm',
   require 'plugins.bufdelete',
   require 'plugins.vim-visual-multi',
+  require 'plugins.windsurf',
+  require 'plugins.origami',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
