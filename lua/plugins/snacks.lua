3e7e70e7534a9d05351c2c19fa89989c966cf82e
Zira3l137 <raulbairamov2002@gmail.com>
2026-06-14 02:10:05 +0400

    feat: add dashboard, remove buffer dimming

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -10,23 +10,33 @@ return {
   --   })
   -- end,
   opts = {
-    dim = {
-      enabled = true,
-      scope = {
-        min_size = 5,
-        max_size = 20,
-        siblings = true,
+    -- dim = {
+    --   enabled = true,
+    --   scope = {
+    --     min_size = 5,
+    --     max_size = 20,
+    --     siblings = true,
+    --   },
+    --   animate = {
+    --     enabled = vim.fn.has 'nvim-0.10' == 1,
+    --     easing = 'outQuad',
+    --     duration = {
+    --       step = 20,   -- ms per step
+    --       total = 300, -- maximum duration
+    --     },
+    --   },
+    --   -- what buffers to dim
+    --   filter = function(buf) return vim.g.snacks_dim ~= false and vim.b[buf].snacks_dim ~= false and
+    --     vim.bo[buf].buftype == '' end,
+    -- },
+    dashboard = {
+      sections = {
+        { section = "header" },
+        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { section = "startup" },
       },
-      animate = {
-        enabled = vim.fn.has 'nvim-0.10' == 1,
-        easing = 'outQuad',
-        duration = {
-          step = 20, -- ms per step
-          total = 300, -- maximum duration
-        },
-      },
-      -- what buffers to dim
-      filter = function(buf) return vim.g.snacks_dim ~= false and vim.b[buf].snacks_dim ~= false and vim.bo[buf].buftype == '' end,
     },
     scroll = {
       enabled = true,
